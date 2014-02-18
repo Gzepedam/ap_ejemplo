666ba816b3b5fb761cefd0c49582d3965adb06b8
Gonzalo Zepeda <gzepedam@prodigy.net.mx>
2014-02-17 19:38:30 -0600

    Primera consolidación

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+ApEjemplo::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ApEjemplo::Application.config.secret_key_base = '5973e0289882b38a3feb0de805db76f4a1b5f41df5e13d121266632b1371d1f6d40abbc9807c33898901a16e47c6d7b99d1ef4963c86d96cc1c9b7dd513c2b3c'
