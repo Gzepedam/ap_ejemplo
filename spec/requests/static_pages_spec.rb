51a4f12f6a620836dc181ff2f5fdcc48b93ac41a
Gonzalo Zepeda <gzepedam@prodigy.net.mx>
2014-02-24 10:16:43 -0600

    Final diagramacion y rutas

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,21 +4,35 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_content('Sample App')
+    it "should have the h1 'Sample App'" do
+      visit root_path
+      page.should have_selector('h1', :text=> 'Sample App')
+    end
+    it "debe tener el titulo basico" do
+      visit root_path
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App")
+    end
+    it "no debe tener titulo propio" do
+      visit root_path
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
   describe "Help Page" do
     it "should have the content 'Help'" do
-      visit 'static_pages/help'
+      visit help_path
       page.should have_content('Help')
     end
   end
   describe "About Page" do
     it "should have the content 'About'" do
-      visit 'static_pages/about'
+      visit about_path
       page.should have_content('About')
+    end
+  end
+  describe "Contact Page" do
+    it "should have the h1 'Contact'" do
+      visit contacat_path
+      page.should have_selector('title', text: "Ruby on Rails Tutorial Sample App | Contact" )
     end
   end
 end
